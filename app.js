c94dedd1c438d573263bf354a78d92a92da5415b
eternal22 <andrey.golovko2904@gmail.com>
2021-12-15 23:22:12 +0200

    New feature added - accent color changer (not final)

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ themeInput.addEventListener('change', () => {
   body.classList.toggle('dark');
 });
 
+// Accent color changer
+const colorInput = document.querySelector('#accent-color');
+colorInput.addEventListener('input', e => {
+  body.style.setProperty('--accent-color', e.target.value);
+});
