120175796b664bfa4e48d28555f9494a38cb5616
eternal22 <andrey.golovko2904@gmail.com>
2021-12-16 15:28:30 +0200

    Added actions flag and mail hover

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const body = document.body;
 
-// Theme toggler
 const themeInput = document.querySelector('#theme');
 themeInput.addEventListener('change', () => {
   body.classList.toggle('dark');
 });
 
-// Accent color changer
 const colorInput = document.querySelector('#accent-color');
 colorInput.addEventListener('input', e => {
   body.style.setProperty('--accent-color', e.target.value);
